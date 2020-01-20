2eeea84dcf83af0b689a86a428862f2a8fe57ef3
pjotr145 <52573376+pjotr145@users.noreply.github.com>
2020-01-20 10:45:57 +0100

    Added yaml settings file and simple fitness calculation.

--- individual.py
+++ individual.py
@@ -15,8 +15,10 @@ class Individual():
         self.rooms = all_rooms
         self.wall_indexes = wall_index_per_room
         self.room_value_index = room_value_index
+        self.fitness = 0
 
         self.gene = np.random.randint(2, size=len(self.walls))
+        # TODO: Make sure the walls given bij the puzzle are set to 1.
         print("Ind gene: {}".format(self.gene))
 
         self.my_rooms = []
@@ -26,3 +28,8 @@ class Individual():
             new_room = Room(self.rooms[i], self.wall_indexes[i])
             self.my_rooms.append(new_room)
 #        print("ind: rooms: {}".format(self.my_rooms))
+
+    def calc_fitness(self):
+        self.fitness = 0
+        for room in self.my_rooms:
+            self.fitness += room.room_score(self.gene)
--- slitherlink.py
+++ slitherlink.py
@@ -3,19 +3,20 @@
 import yaml
 from hulp import get_file_name, get_walls_and_rooms
 from hulp import get_wall_indices_per_room, find_index_no_nine
+from hulp import find_index_all_ones
 from population import Population
 
 FILE_EXPR = "puzzle*.txt"
 SETTINGS_FILE = "settings.yaml"
 
+# Read the initial settings for the GA
+with open(SETTINGS_FILE) as f:
+    SETTINGS = yaml.load(f, Loader=yaml.FullLoader)
+
 # Read the puzzle
 filename = get_file_name(FILE_EXPR)
 with open(filename) as f:
     content = f.read().splitlines()
-
-# Read the initial settings for the GA
-with open(SETTINGS_FILE) as f:
-    SETTINGS = yaml.load(f, Loader=yaml.FullLoader)
 
 all_walls, all_rooms = get_walls_and_rooms(content)
 wall_index_per_room = get_wall_indices_per_room(len(content))
@@ -34,3 +35,5 @@ room_with_value_index = find_index_no_nine(all_rooms)
 
 population = Population(all_walls, all_rooms, wall_index_per_room,
                         room_with_value_index, SETTINGS)
+print("Walls: {}".format(all_walls))
+print("Walls: {}".format(find_index_all_ones(all_walls)))
